a206ec0207917a1e3589bfb41bacd6fe32bb391e
akash-2414023 <mahatoakash2004@gmail.com>
2026-09-12 22:31:36 +0530

    Time: 22 ms (12.79%), Space: 35 MB (11.6%) - LeetHub

---
+++ 4048-count-values-with-equally-spaced-occurrences-i/4048-count-values-with-equally-spaced-occurrences-i.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int countSpecialIntegers(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int, vector<int>> mp;
+        for (int i = 0; i < n; i++) {
+            mp[nums[i]].push_back(i);
+        }
+        int count = 0;
+        for (auto it : mp) {
+            vector<int> vec = it.second;
+            if (vec.size() == 3 && 2 * vec[1] == vec[0] + vec[2]) {
+                count++;
+            }
+        }
+        return count;
+    }
+};
